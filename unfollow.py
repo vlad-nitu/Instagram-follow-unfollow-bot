26557a5519c6e5b91f42661590ce405e4fafe8dd
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-12-26 20:17:00 +0200

    Modified unfollower.py

--- unfollow.py
+++ unfollow.py
@@ -5,8 +5,8 @@ import random
 import time
 
 
-def sleep_for_period_of_time():
-    limit = random.randint(40, 60)
+def sleep_for_period_of_time(a, b):
+    limit = random.randint(a, b)
     time.sleep(limit)
 
 
@@ -23,18 +23,18 @@ def main():
 
     cookies_button = browser.find_element(By.XPATH, "//button[@class='_a9-- _a9_0']")
     cookies_button.click()
-    sleep_for_period_of_time()
+    sleep_for_period_of_time(5, 10)
 
     username_input = browser.find_element(By.CSS_SELECTOR, "input[name='username']")
     password_input = browser.find_element(By.CSS_SELECTOR, "input[name='password']")
 
     username_input.send_keys(user)
     password_input.send_keys(pwd)
-    sleep_for_period_of_time()
+    sleep_for_period_of_time(2, 4)
 
     login_button = browser.find_element(By.XPATH, "//button[@type='submit']")
     login_button.click()
-    sleep_for_period_of_time()
+    sleep_for_period_of_time(2, 4)
 
     # not_now = browser.find_element_by_xpath("//div[@class='cmbtv']/button")
     # not_now.click()
@@ -42,17 +42,20 @@ def main():
 
     ig_user = input("Enter your instagram username: ")
     browser.get(f"https://www.instagram.com/{ig_user}")
-    sleep_for_period_of_time()
+    sleep_for_period_of_time(5, 10)
 
-    followers_link = browser.find_element(By.XPATH, "//ul/li[3]/a")
-    followers_link.click()
-    sleep_for_period_of_time()
+    browser.find_element(By.PARTIAL_LINK_TEXT, "following").click()
+    sleep_for_period_of_time(2, 4)
 
     num_unfollow = input("How many person you want to unfollow: ")
+    sleep_for_period_of_time(2, 4)
 
-    while (True):
+    pop_up_window = browser.find_element(By.XPATH, "//div[@class='_aano']")
+    i = 0
+
+    while True:
         try:
-            i = 0
+
             list_of_following = browser.find_elements(By.XPATH, '//button/div/div[contains(text(), "Following")]')
             for person in list_of_following:
                 if person.text == "Following":
@@ -63,21 +66,23 @@ def main():
                     print("Unfollowed!")
                     i += 1
                     print(i)
-                    sleep_for_period_of_time()
+                    sleep_for_period_of_time(3, 10) # larger
                 else:
                     pass
-                if i >= int(num_unfollow):
-                    break
-
-            browser.execute_script("arguments[0].scrollIntoView(true);", list_of_following[i])
-
-            answer = input("The programm finished! Click on 'e' to exit.. ")
-            if answer.lower().startswith("e"):
-                browser.quit()
-                exit()
-
+            if i >= int(num_unfollow):
+                break
+            else:
+                browser.execute_script(
+                    'arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;',
+                    pop_up_window)
+                time.sleep(1)
         except Exception as e:
             print(e)
+
+    answer = input("The program finished! Click on 'e' to exit.. ")
+    if answer.lower().startswith("e"):
+        browser.quit()
+        exit()
 
 
 if __name__ == "__main__":
